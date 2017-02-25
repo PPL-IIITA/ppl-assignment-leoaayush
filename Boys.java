3acb6f34a43dd818726b8573a7859e00b099df59
Aayush Goyal <iit2015123@iiita.ac.in>
2017-02-26 02:00:23 +0530

    Add files via upload

---
+++ Boys.java
@@ -0,0 +1,19 @@
+package ppl;
+
+public class Boys{
+
+	
+
+	
+
+	public	String nameb;
+	public	int attb;
+	
+	public int  intelb;
+	public int bud;
+	public int minattract;
+	public int flagb=0;
+
+	public int type;
+
+}
